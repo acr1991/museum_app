70759d2a8022133bac09eeebb850c9973167c4e6
acr1991 <acr1991@live.nl>
2020-01-31 14:54:25 +0100

    Adjustments made to exisiting code. App is now able to connect to details page

--- detail-page.js
+++ detail-page.js
@@ -6,32 +6,22 @@ function submitComment() {
   console.log(name);
   console.log(msg);
 }
-// add event handler
-function submitComment() {
-  // gather data
 
+function submitComment() {
   const inputField = document.getElementById("name");
   const name = inputField.value;
   const textArea = document.getElementById("msg");
   const msg = textArea.value;
-  // create the elements
-
   const comment = document.createElement("section");
   const h3 = document.createElement("h3");
   const p = document.createElement("p");
-  // adjust the elements we created
-
   h3.innerHTML = `${name} said:`;
   p.innerHTML = msg;
   comment.classList.add("comment");
   comment.appendChild(h3);
   comment.appendChild(p);
-  // display the elements on the page
-
   const commentSection = document.getElementById("comments");
   commentSection.appendChild(comment);
-  // reset form values
-
   inputField.value = null;
   textArea.value = null;
 }
